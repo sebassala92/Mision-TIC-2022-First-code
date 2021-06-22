25985a3201b7cadfd35446d235a48a51b3f21d3b
sebassala92 <78507604+sebassala92@users.noreply.github.com>
2021-06-22 00:29:40 -0500

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-matriz_1 = []
-
-for fila in range(3):
-    matriz_1.append([])
-    latitud = float(input('ingrese la latitud'))
-    matriz_1[fila].append(latitud)
-    longitud = float(input('ingrese la longitud'))
-    matriz_1[fila].append(longitud)
